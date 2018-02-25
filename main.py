d7aa543d08ea3a1e993a65c7242fb7d6b608d2ac
palance <palanceli@163.com>
2018-02-25 23:31:32 +0800

    commit changes

---
+++ main.py
@@ -0,0 +1,21 @@
+
+# -*- coding:utf-8 -*-
+
+import logging
+import os
+import unittest
+from selenium import webdriver
+
+class Main(unittest.TestCase):
+    def setUp(self):
+        logFmt = '%(asctime)s %(lineno)04d %(levelname)-8s %(message)s'
+        logging.basicConfig(level=logging.DEBUG, format=logFmt, datefmt='%H:%M',)
+
+    def case1(self, imgName, img):
+        option = webdriver.ChromeOptions()
+
+if __name__ == '__main__':
+    logFmt = '%(asctime)s %(lineno)04d %(levelname)-8s %(message)s'
+    logging.basicConfig(level=logging.DEBUG, format=logFmt, datefmt='%H:%M',)
+    unittest.main
+    # cmd: python -m main Main.case1
