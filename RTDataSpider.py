2e60c12dae9569e970f7a3d0bc399ac00621d6c4
palanceli <palanceli@163.com>
2020-01-19 21:44:56 +0800

    update changes

--- RTDataSpider.py
+++ RTDataSpider.py
@@ -5,14 +5,10 @@ import logging
 import os
 import unittest
 import json
-import bs4
 import urllib
 import urllib.request
 import re
 import datetime
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-from matplotlib.dates import strpdate2num
 import numpy as np
 
 class RTDataSpider(object):
@@ -31,12 +27,17 @@ class QQRTDataSplider(RTDataSpider):
         RTDataSpider.__init__(self)
         self.rtData = [
         {'name':'搜狗',           'symbol':'SOGO',        'rtData':0.0, 'requestid':'usSOGO'},
-        {'name':'AMD',           'symbol':'AMD',        'rtData':0.0, 'requestid':'usAMD'},
-        {'name':'NTES',           'symbol':'NTES',        'rtData':0.0, 'requestid':'usNTES'},
-        {'name':'BABA',           'symbol':'BABA',        'rtData':0.0, 'requestid':'usBABA'},
-        {'name':'APPLE',           'symbol':'APPLE',        'rtData':0.0, 'requestid':'usAAPL'},
+        {'name':'AMD',           'symbol':'AMD',         'rtData':0.0, 'requestid':'usAMD'},
+        {'name':'TSLA',           'symbol':'TSLA',       'rtData':0.0, 'requestid':'usTSLA'},
+        {'name':'NTES',           'symbol':'NTES',       'rtData':0.0, 'requestid':'usNTES'},
+        {'name':'BABA',           'symbol':'BABA',       'rtData':0.0, 'requestid':'usBABA'},
+        {'name':'PG',             'symbol':'PG',         'rtData':0.0, 'requestid':'usPG'},
+        {'name':'JD',             'symbol':'JD',         'rtData':0.0, 'requestid':'usJD'},
+        {'name':'APPLE',          'symbol':'APPLE',     'rtData':0.0, 'requestid':'usAAPL'},
         {'name':'小米',           'symbol':'01810.HK',    'rtData':0.0, 'requestid':'r_hk01810'},
-        {'name':'腾讯',           'symbol':'700.HK',         'rtData':0.0, 'requestid':'r_hk00700'},
+        {'name':'腾讯',           'symbol':'700.HK',      'rtData':0.0, 'requestid':'r_hk00700'},
+        {'name':'美团',           'symbol':'3690.HK',     'rtData':0.0, 'requestid':'r_hk03690'},
+        {'name':'波司登',          'symbol':'3998.HK',     'rtData':0.0, 'requestid':'r_hk03998'},
         {'name':'标普500',        'symbol':'SPY',         'rtData':0.0, 'requestid':'usSPY'},
         {'name':'纳斯达克100',     'symbol':'QQQ',         'rtData':0.0, 'requestid':'usQQQ'},
         {'name':'沪深300',        'symbol':'000300',      'rtData':0.0, 'requestid':'sh000300'},
